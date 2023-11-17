b543e9a318a407fb8c834e5b1b9c99dd215f550f
Adam Alexander <46898882+aja26@users.noreply.github.com>
2023-11-17 23:03:40 +0000

    Update experience.js

--- src/Experience/experience.js
+++ src/Experience/experience.js
@@ -2,6 +2,12 @@ import * as THREE from 'three'
 import Sizes from "./Utils/Sizes.js";
 import Time from "./Utils/Time.js";
 import Camera from "./Camera.js";
+import Renderer from './Renderer.js';
+import World from './World/World.js';
+import Resources from './Utils/Resources.js';
+import Debug from './Utils/Debug.js';
+import sources from './sources.js';
+
 
 let instance = null;
 
@@ -20,10 +26,15 @@ export default class Experience{
         this.canvas = canvas
 
         // Setup
+        this.debug = new Debug();
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+        this.resources = new Resources(sources);
         this.camera = new Camera();
+        this.renderer = new Renderer();
+        this.world = new World();
+
 
         // Sizes resize event
         this.sizes.on('resize', () => {
@@ -37,13 +48,14 @@ export default class Experience{
         })
     }
 
-     resize(){
-         this.camera.resize();
-        
+    resize(){
+       this.camera.resize();
+       this.renderer.resize();  
     }
 
     update(){
        this.camera.update();
-
+       this.world.update();
+       this.renderer.update();
     }
 }
