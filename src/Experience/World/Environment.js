02d539222d4b751a3b11df4bc4643759ce2dcf0b
Adam Alexander <46898882+aja26@users.noreply.github.com>
2023-11-18 23:51:17 +0000

    Update Environment.js

--- src/Experience/World/Environment.js
+++ src/Experience/World/Environment.js
@@ -5,7 +5,13 @@ export default class Environment{
     constructor(){
         this.experience = new Experience();
         this.scene = this.experience.scene;
-        this.resources = this.experience.resources
+        this.resources = this.experience.resources;
+        this.debug = this.experience.debug;
+
+        // Debug
+        if(this.debug.active){
+            this.debugFolder = this.debug.ui.addFolder('environment')
+        }
 
         this.setSunlight();
         this.setEnvironmentMap();
@@ -28,16 +34,27 @@ export default class Environment{
         this.environmentMap.texture.colorSpace = THREE.SRGBColorSpace
         this.scene.environment = this.environmentMap.texture;
 
-        this.setEnvironmentMap.updateMaterial = () => {
+        this.environmentMap.updateMaterials = () => {
             this.scene.traverse((child) => {
                 if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial){
                     child.material.envMap = this.environmentMap.texture
-                    child.material.envMapInensity = this.environmentMap.intensity
+                    child.material.envMapIntensity = this.environmentMap.intensity
                     child.material.needsUpdate = true;
                 }
             })
         }
 
-        this.setEnvironmentMap.updateMaterial();
+        this.environmentMap.updateMaterials();
+
+        // Debug
+        if(this.debug.active){
+            this.debugFolder
+                .add(this.environmentMap, 'intensity')
+                .name('envMapIntensity')
+                .min(0)
+                .max(4)
+                .step(0.001)
+                .onChange(this.environmentMap.updateMaterials)
+        }
     }
 }
